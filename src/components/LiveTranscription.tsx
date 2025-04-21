e872ec4f0285f4cce23849f8faedadd05834adc2
reonzpika-gmailcom <reonzpika@gmail.com>
2025-04-21 23:46:45 +1200

    docs: add section about handling multiple features

---
+++ src/components/LiveTranscription.tsx
@@ -0,0 +1,9 @@
+// Work in progress on live transcription
+export function LiveTranscription() {
+  return (
+    <div>
+      <h1>Live Transcription Component</h1>
+      {/* Work in progress */}
+    </div>
+  );
+} 
