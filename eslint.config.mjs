b6244b8124aa7599e3827b89ee808f9a7b82e54c
reonzpika-gmailcom <reonzpika@gmail.com>
2025-06-25 23:54:00 +1200

    updated ESlint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,62 +7,70 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tailwind from 'eslint-plugin-tailwindcss';
 import testingLibrary from 'eslint-plugin-testing-library';
 
-export default antfu({
-  react: true,
-  typescript: true,
-
-  lessOpinionated: true,
-  isInEditor: false,
-
-  stylistic: {
-    semi: true,
+export default antfu(
+  {
+    react: true,
+    typescript: true,
+    lessOpinionated: true,
+    isInEditor: false,
+    stylistic: {
+      semi: true,
+    },
+    formatters: {
+      css: true,
+    },
+    ignores: [
+      'migrations/**/*',
+      'next-env.d.ts',
+      'node_modules/**/*',
+      '.next/**/*',
+      'out/**/*',
+    ],
   },
-
-  formatters: {
-    css: true,
+  ...tailwind.configs['flat/recommended'],
+  jsxA11y.flatConfigs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
   },
-
-  ignores: [
-    'migrations/**/*',
-    'next-env.d.ts',
-  ],
-}, ...tailwind.configs['flat/recommended'], jsxA11y.flatConfigs.recommended, {
-  plugins: {
-    '@next/next': nextPlugin,
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
   },
-  rules: {
-    ...nextPlugin.configs.recommended.rules,
-    ...nextPlugin.configs['core-web-vitals'].rules,
+  {
+    files: [
+      '**/*.test.ts?(x)',
+    ],
+    ...testingLibrary.configs['flat/react'],
+    ...jestDom.configs['flat/recommended'],
   },
-}, {
-  plugins: {
-    'simple-import-sort': simpleImportSort,
+  {
+    files: [
+      '**/*.spec.ts',
+      '**/*.e2e.ts',
+    ],
+    ...playwright.configs['flat/recommended'],
   },
-  rules: {
-    'simple-import-sort/imports': 'error',
-    'simple-import-sort/exports': 'error',
+  {
+    rules: {
+      'import/order': 'off', // Avoid conflicts with `simple-import-sort` plugin
+      'sort-imports': 'off', // Avoid conflicts with `simple-import-sort` plugin
+      'style/brace-style': ['error', '1tbs'], // Use the default brace style
+      'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
+      'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
+      'node/prefer-global/process': 'off', // Allow using `process.env`
+      'test/padding-around-all': 'error', // Add padding in test files
+      'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
+    },
   },
-}, {
-  files: [
-    '**/*.test.ts?(x)',
-  ],
-  ...testingLibrary.configs['flat/react'],
-  ...jestDom.configs['flat/recommended'],
-}, {
-  files: [
-    '**/*.spec.ts',
-    '**/*.e2e.ts',
-  ],
-  ...playwright.configs['flat/recommended'],
-}, {
-  rules: {
-    'import/order': 'off', // Avoid conflicts with `simple-import-sort` plugin
-    'sort-imports': 'off', // Avoid conflicts with `simple-import-sort` plugin
-    'style/brace-style': ['error', '1tbs'], // Use the default brace style
-    'ts/consistent-type-definitions': ['error', 'type'], // Use `type` instead of `interface`
-    'react/prefer-destructuring-assignment': 'off', // Vscode doesn't support automatically destructuring, it's a pain to add a new variable
-    'node/prefer-global/process': 'off', // Allow using `process.env`
-    'test/padding-around-all': 'error', // Add padding in test files
-    'test/prefer-lowercase-title': 'off', // Allow using uppercase titles in test titles
-  },
-});
+);
