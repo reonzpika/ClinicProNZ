942b21584575461d4f563f95b39b46e54ba3e77b
reonzpika-gmailcom <reonzpika@gmail.com>
2025-11-06 22:56:29 +1300

    deleted some unused files

--- fix_api_provider_column.sql
+++
@@ -1,5 +0,0 @@
--- Add api_provider column if it doesn't exist
-ALTER TABLE api_usage_costs ADD COLUMN IF NOT EXISTS api_provider text NOT NULL DEFAULT 'unknown';
-
--- Update the column to be NOT NULL without default for future inserts
-ALTER TABLE api_usage_costs ALTER COLUMN api_provider DROP DEFAULT;
